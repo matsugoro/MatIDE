138bea331bc07f4d21e63bfccbf81e8fa7509c7d
Matsugoro <matsugoro@live.jp>
2015-04-17 21:54:21 +0900

    0417_2

---
+++ MatIDE/ViewModels/ProjectExplorerVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Livet;
+using MatIDE.ViewModels.Explorer;
+using MatIDE.Docking;
+using MatIDE.ViewModels.Dock;
+
+namespace MatIDE.ViewModels
+{
+	public class ProjectExplorerVM : ToolViewModel
+	{
+//		ExplorerNodeVMCollection<ExplorerNodeVM>	_rootItems;
+
+		public ProjectExplorerVM() : base("ProjectFileView", "TPaneRight")
+		{
+			ContentId = "ProjectExplorer";
+		}
+
+		/*
+		public ExplorerNodeVMCollection<ExplorerNodeVM> RootItems
+		{
+			get {
+				if ( _rootItems==null ){
+					_rootItems = new ExplorerNodeVMCollection<ExplorerNodeVM>();
+					_rootItems.Add(new ExplorerSFNodesVM(MatIDE.Native.Win32Shell.FOLDERID_Links));
+					_rootItems.Add(new ExplorerPCNodeVM() );
+				}
+				return _rootItems;
+			}
+		}
+		*/
+
+	}
+}
